59fcbbeffb3d7eaf9bb390298cb9c1025fbb438d
ajoy267 <andrew.joy12@hotmail.com>
2023-01-24 19:43:55 -0700

    starting to add some file structure

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../../components/Control/Footer';
+import Header from '../../components/Control/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
